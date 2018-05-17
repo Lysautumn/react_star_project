0fcded49d754729eec9dddb81b92aea8236e0a8f
Ally <ally@primeacademy.io>
2018-05-17 14:57:48 -0500

    before adding in inputs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      starList: [
+        {name:'hadar', diameter: 50},
+        {name:'gacrux', diameter: 2000},
+        {name:'kochab', diameter: 555}
+      ],
+      newStar: {
+        name: '',
+        diameter: '',
+      }
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDiameterChange = this.handleDiameterChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    console.log(event.target.value);
+    
+  }
+
+  handleDiameterChange(event) {
+    console.log(event.target.value);
+    
   }
   render() {
+    // let starListItemArray = [];
+    // for(let i = 0; i < this.state.starList.length; i++) {
+    //   const starListItem = <li>{this.state.starList[i]}</li>
+    //   starListItemArray.push(starListItem);
+    // }
+
+    // const starListItemArray = this.state.starList.map(star => <li>{star}</li>);
     return (
-      null
+      <div>
+        <div>
+            <input onChange={this.handleNameChange} value={this.state.newStar.name}/>
+            <input onChange={this.handleDiameterChange} value={this.state.newStar.diameter}/>
+        </div>
+        {this.state.newStar.name} and {this.state.newStar.diameter}
+        <ul>
+          {this.state.starList.map(star => <li key={star.name}>The star {star.name} has a diameter of {star.diameter}</li>)}
+        </ul>
+      </div>
     );
   }
 }
